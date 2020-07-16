2ad40f87a10678b057226fc6c411fe69799324f5
wiseman-why-not <marcus.e.wiseman@gmail.com>
2020-07-15 21:36:45 -0500

    added the kanban module which is another lazy-loaded feature module for the development of the kanban boards.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './user/auth.guard';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,7 +6,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule) }
+  { path: 'login', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+  { path: 'kanban', loadChildren: () => import('./kanban/kanban.module').then(m => m.KanbanModule), canActivate: [AuthGuard]},
 ];
 
 @NgModule({
